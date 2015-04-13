92de1e8a1976ab2b42e4dfb971e0d7599fd4fb42
B.Thieurmel <bthieurmel@gmail.com>
2015-04-13 14:59:10 +0200

    Add Function.prototype.bind to fix RStudio viewer bug in windows desktop

--- inst/htmlwidgets/visNetwork.js
+++ inst/htmlwidgets/visNetwork.js
@@ -1,3 +1,30 @@
+// Add shim for Function.prototype.bind() from:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+// for fix some RStudio viewer bug (Desktop / windows)
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+    // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+            return fToBind.apply(this instanceof fNOP && oThis
+                                               ? this
+                                               : oThis,
+                                             aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 HTMLWidgets.widget({
 
   name: 'visNetwork',
@@ -12,7 +39,6 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
-    
     //legend  
     if(x.groups && x.legend){
       var legend = document.createElement('div');
@@ -302,6 +328,8 @@ HTMLWidgets.widget({
         callback(data);
 
       }
+      
+      
   },
 
   resize: function(el, width, height, instance) {
