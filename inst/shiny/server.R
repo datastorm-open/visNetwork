6a33600169156984d03c1eba06fe503ac631d2a6
B.Thieurmel <bthieurmel@gmail.com>
2015-04-17 14:11:55 +0200

    Update shiny example for 0.0.2

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -4,25 +4,42 @@ require(visNetwork)
 shinyServer(function(input, output) {
   
   output$network <- renderVisNetwork({
-    if(input$goButton >0){
-      isolate({
-        nb = input$nb
-        nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
-                            group = sample(1:10, nb, replace = TRUE), value = 1:nb,
-                            title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
-        
-        edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
-                            to = trunc(runif(nb)*(nb-1))+1,
-                            value = rnorm(nb, 10), label = paste("Edge", 1:nb),
-                            title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
-        
-        # custom navigation
-        visNetwork(nodes, edges, legend = input$legend2, id.selection = TRUE, height = "500px") %>% 
-          visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE)
-      })
-    }
+    input$goButton
+    isolate({
+      nb = input$nb
+      nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                          group = sample(1:10, nb, replace = TRUE), value = 1:nb,
+                          title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+      
+      edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
+                          to = trunc(runif(nb)*(nb-1))+1,
+                          value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                          title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+      
+      # custom navigation
+      visNetwork(nodes, edges, legend = input$legend2, height = "500px") %>% 
+        visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE, nodesIdSelection = TRUE)
+    })
 
+})
 
-  })
+output$network2 <- renderVisNetwork({
   
+  nb = 5
+  nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                      group = sample(1:10, nb, replace = TRUE), value = 1:nb,
+                      title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+  
+  edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
+                      to = trunc(runif(nb)*(nb-1))+1,
+                      value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                      title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+  
+  # custom navigation
+  visNetwork(nodes, edges, height = "500px") %>% 
+    visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE, nodesIdSelection = TRUE)
+  
+  
+  
+})
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -6,6 +6,9 @@ shinyUI(fluidPage(
      sliderInput("nb", "number of nodes : ", min = 2, max = 1000, value = 10),
      actionButton("goButton", "Go!"),
      checkboxInput("legend2", "legend", value = TRUE),
-     visNetworkOutput("network")
+     visNetworkOutput("network", height = "505px"),
+     hr(),
+     div(h3("Just to test two networks on same apps"), align = "center"),
+     visNetworkOutput("network2", height = "500px")
     
 ))
