e77dd302a4b34d66877858cf9689ec74d106248e
bthieurmel <bthieurmel@gmail.com>
2015-05-06 11:33:08 +0200

    Update shiny app for selection test

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -21,26 +21,31 @@ shinyServer(function(input, output) {
         visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE, 
                    highlightNearest = TRUE, nodesIdSelection = TRUE)
     })
-
-})
-
-output$network2 <- renderVisNetwork({
+    
+  })
   
-  nb = 5
-  nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
-                      group = sample(1:10, nb, replace = TRUE), value = 1:nb,
-                      title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+  output$network2 <- renderVisNetwork({
+    
+    nb = 5
+    nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                        group = sample(1:10, nb, replace = TRUE), value = 1:nb,
+                        title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+    
+    edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
+                        to = trunc(runif(nb)*(nb-1))+1,
+                        value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                        title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+    
+    # custom navigation
+    visNetwork(nodes, edges) %>% 
+      visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE, nodesIdSelection = TRUE)
+    
+    
+    
+  })
   
-  edges <- data.frame(from = trunc(runif(nb)*(nb-1))+1,
-                      to = trunc(runif(nb)*(nb-1))+1,
-                      value = rnorm(nb, 10), label = paste("Edge", 1:nb),
-                      title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+  output$test <- renderText({
+    input$network_selected
+  })
   
-  # custom navigation
-  visNetwork(nodes, edges) %>% 
-    visOptions(navigation = TRUE, dataManipulation = TRUE, stabilize = FALSE, nodesIdSelection = TRUE)
-  
-  
-  
-})
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -6,6 +6,8 @@ shinyUI(fluidPage(
      sliderInput("nb", "number of nodes : ", min = 2, max = 1000, value = 10),
      actionButton("goButton", "Go!"),
      checkboxInput("legend2", "legend", value = TRUE),
+     h4("input$network_selected"),verbatimTextOutput("test"),
+     h4("output$network"),
      visNetworkOutput("network",height = "600px"),
      hr(),
      div(h3("Just to test two networks on same apps"), align = "center"),
