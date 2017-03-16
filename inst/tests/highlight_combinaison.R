99276b4303e34ecbc6e507c7eea46563003b1644
B.Thieurmel <bthieurmel@gmail.com>
2017-03-16 18:33:21 +0100

    add some script test

--- inst/tests/highlight_combinaison.R
+++ inst/tests/highlight_combinaison.R
@@ -1,20 +1,59 @@
 # simple nodes, passing some information individually
-nodes <- data.frame(id = 1:5, color.background = c("blue", NA, NA, NA, NA), 
-                    color.highlight = c(NA,"purple", NA, NA, NA), 
+nodes <- data.frame(id = 1:5, color = c("blue", NA, "green", NA, NA))
+
+edges <- data.frame(from = trunc(runif(5)*(5-1))+1,
+                    to = trunc(runif(5)*(5-1))+1)
+
+visNetwork(nodes, edges) %>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 10, hover = F))
+
+
+# simple nodes, passing some information individually
+nodes <- data.frame(id = 1:5, 
+                    color.highlight = c(NA,"purple", "black", NA, NA))
+
+edges <- data.frame(from = trunc(runif(5)*(5-1))+1,
+                    to = trunc(runif(5)*(5-1))+1)
+
+visNetwork(nodes, edges) %>% 
+  visNodes(color = "red")%>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 10, hover = F), collapse = T)
+
+# simple nodes, passing some information individually
+nodes <- data.frame(id = 1:5, 
                     group = c("B", "B", "A", "C", "A"))
 
 edges <- data.frame(from = trunc(runif(5)*(5-1))+1,
                     to = trunc(runif(5)*(5-1))+1)
 
-visNetwork(nodes, edges) %>% visOptions(highlightNearest = list(enabled = T, degree = 10), selectedBy = "group")
+visNetwork(nodes, edges) %>% 
+  visGroups(groupname = "A", shape = "ellipse", color = list(background = "green", highlight = "red")) %>%
+  visGroups(groupname = "B", shape = "square", color = list(border = "blue", highlight = "purple")) %>%
+  visOptions(highlightNearest = list(enabled = T, degree = 10, hover = T), selectedBy = "group")
 
-# icons in group and individually
+# simple nodes unsing visNodes
+nodes <- data.frame(id = 1:5, color.border = c("red","purple", "black", NA, NA))
+
+edges <- data.frame(from = trunc(runif(5)*(5-1))+1,
+                    to = trunc(runif(5)*(5-1))+1)
+
+visNetwork(nodes, edges) %>% 
+  visNodes(shape = "square", color = "green")  %>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 10, hover = T))
+
+# simple nodes individually + visNodes
+nodes <- data.frame(id = 1:5, color = c(NA, NA, "blue", NA, "red"))
+
+edges <- data.frame(from = trunc(runif(5)*(5-1))+1,
+                    to = trunc(runif(5)*(5-1))+1)
+
+visNetwork(nodes, edges) %>% 
+  visNodes(shape = "square", color = list(background = "green", border = "purple"))  %>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 10, hover = T))
+
+# icons in group only
 ?addFontAwesome
-
-nodes <- data.frame(id = 1:3, group = c("B", "A", "B"), 
-                    shape = "icon", 
-                    icon.code = c("f0c0", NA, NA),
-                    icon.color = c(NA, NA, "orange"))
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"))
 edges <- data.frame(from = c(1,2), to = c(2,3))
 
 visNetwork(nodes, edges) %>%
@@ -22,13 +61,44 @@ visNetwork(nodes, edges) %>%
   visGroups(groupname = "B", shape = "icon", icon = list(code = "f007", color = "red")) %>%
   addFontAwesome() %>% visOptions(highlightNearest = list(enabled = T, hover = T), selectedBy = "group")
 
+# icons individually only
+?addFontAwesome
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"), 
+                    shape = "icon", 
+                    icon.code = c("f0c0", "f007", "f007"),
+                    icon.color = c("red", NA, "orange"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges) %>%
+  addFontAwesome() %>% visOptions(highlightNearest = list(enabled = T, hover = T), selectedBy = "group")
+
+# icons in group and individually
+?addFontAwesome
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"), 
+                    shape = "icon", 
+                    icon.code = c("f0c0", NA, NA),
+                    icon.color = c(NA, NA, "orange"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges) %>%
+  visNodes(icon = list(code = "f0c0", color = "purple")) %>%
+  visGroups(groupname = "B", shape = "icon", icon = list(code = "f007", color = "red")) %>%
+  addFontAwesome() %>% visOptions(highlightNearest = list(enabled = T, hover = T), selectedBy = "group")
+
 # test passing shape information globally
+nodes <- data.frame(id = 1:3, icon.code = c("f0c0", "f0c0","f007"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges) %>%
+  visNodes(shape = "icon", icon = list(color = "red")) %>%
+  addFontAwesome() %>% visOptions(highlightNearest = T, selectedBy = "id")
+
 nodes <- data.frame(id = 1:3, icon.code = c("f0c0", "f0c0","f007"),
                     icon.color = c("orange", NA, NA))
 edges <- data.frame(from = c(1,2), to = c(2,3))
 
 visNetwork(nodes, edges) %>%
-  visNodes(shape = "icon") %>%
+  visNodes(shape = "icon", icon = list(color = "red")) %>%
   addFontAwesome() %>% visOptions(highlightNearest = T, selectedBy = "id")
 
 # with image
@@ -46,15 +116,15 @@ visNetwork(nodes, edges, width = "100%") %>%
   visLayout(randomSeed = 2) %>% visOptions(highlightNearest = T, selectedBy = "group")
 
 # with image, passing globally
-nodes <- data.frame(id = 1:4, color = "red",
+nodes <- data.frame(id = 1:4, color.border = c("red",NA, NA, "blue"),
                     image = paste0(path_to_images, 1:4, ".png"),
                     label = "I'm an image")
 
 edges <- data.frame(from = c(2,4,3,3), to = c(1,2,4,2))
 
 visNetwork(nodes, edges, width = "100%") %>% 
-  visNodes(shapeProperties = list(useBorderWithImage = TRUE, useImageSize=TRUE), shape = "image") %>%
-  visLayout(randomSeed = 2) %>% visOptions(highlightNearest = list(enabled = TRUE, hover= T), selectedBy = "group")
+  visNodes(shapeProperties = list(useBorderWithImage = TRUE, useImageSize=TRUE), shape = "image", color = "orange") %>%
+  visLayout(randomSeed = 2) %>% visOptions(highlightNearest = list(enabled = TRUE, hover= T))
 
 
 # test on legend passing icons in a list
